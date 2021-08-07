bc5eff96875246bbe98567deda863a0335eb2cba
Harshit Nigdikar <harshitnigdikar@gmail.com>
2021-08-08 03:16:51 +0530

    Completed the left side header

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,20 @@
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
 function Header() {
     return (
         <div className = 'header'>
-            <h1>This is the header</h1>
 
             <div className = "header__left">
+                <img src = "https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt= ""/>
+
+                <div className = "header__search">
+                    <SearchIcon/>
+                    <input type = "text" />
+                </div>
 
             </div>
 
-            <div className = "header__right">
-                
+            <div className = "header__right">    
             </div>
             
         </div>
---
+++ src/HeaderOption.js
@@ -0,0 +1,11 @@
+import './HeaderOption.css';
+
+function HeaderOption() {
+    return (
+        <div className = "headerOption">
+            
+        </div>
+    )
+}
+
+export default HeaderOption
