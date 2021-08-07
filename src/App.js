27d783ff62841788a64373811e4dc84b38409e14
Harshit Nigdikar <harshitnigdikar@gmail.com>
2021-08-07 20:38:50 +0530

    Basic App Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets Build a LinkedIn Clone</h1>
 
-      {/* Header*/}
+       {/* Header*/}
+      <Header/>
 
       {/* App body*/}
 
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,16 @@
+import './Header.css'
 function Header() {
     return (
-        <div>
+        <div className = 'header'>
+            <h1>This is the header</h1>
+
+            <div className = "header__left">
+
+            </div>
+
+            <div className = "header__right">
+                
+            </div>
             
         </div>
     )
