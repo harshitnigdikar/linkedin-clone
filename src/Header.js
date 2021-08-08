c9d551e8425d4d2f0b87331cbe77127e9eed1525
Harshit Nigdikar <harshitnigdikar@gmail.com>
2021-08-08 23:29:19 +0530

    Completed designing Header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,13 @@
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+
+import  BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
 function Header() {
     return (
         <div className = 'header'>
@@ -14,7 +22,12 @@ function Header() {
 
             </div>
 
-            <div className = "header__right">    
+            <div className ="header__right"> 
+            <HeaderOption Icon = {HomeIcon} title = "Home" />
+            <HeaderOption Icon = {SupervisorAccountIcon} title = "My Network" />
+            <HeaderOption Icon = {BusinessCenterIcon} title = "Jobs" />
+            <HeaderOption Icon = {ChatIcon} title = "Messaging" />
+            <HeaderOption Icon = {NotificationsIcon} title = "Notifications" />  
             </div>
             
         </div>
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,11 +1,12 @@
 import './HeaderOption.css';
 
-function HeaderOption() {
+function HeaderOption({Icon , title }) {
     return (
         <div className = "headerOption">
-            
+            {Icon && <Icon className = "headerOption__icon" />}
+            <h3 className = "headerOption__title">{title}</h3>
         </div>
-    )
+    );
 }
 
 export default HeaderOption
